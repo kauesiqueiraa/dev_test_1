20ce3ea88e174e53bffc7cb1980148839c627bc6
Lucas Xavier <lsantx@gmail.com>
2022-03-27 21:58:08 -0300

    update assert

--- calculator.py
+++ calculator.py
@@ -1,5 +1,3 @@
-
-
 def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tuple:
     """
     returns a tuple of floats contained anual savings, monthly savings, applied_discount and coverage
@@ -8,7 +6,7 @@ def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tupl
     monthly_savings = 0
     applied_discount = 0
     coverage = 0
-    
+
     # your code here #
 
     return (
@@ -23,66 +21,43 @@ if __name__ == "__main__":
     print("Testing...")
 
     assert calculator([1518, 1071, 968], 0.95871974, "Industrial") == (
-        2837.26,
-        236.44,
+        2836.11,
+        236.34,
         0.12,
         0.9,
     )
 
     assert calculator([1000, 1054, 1100], 1.12307169, "Residencial") == (
-        3712.19,
-        309.35,
-        0.18,
-        0.9,
+        3710.84, 309.24, 0.18, 0.9,
     )
 
     assert calculator([973, 629, 726], 1.04820025, "Comercial") == (
-        2381.65,
-        198.47,
-        0.16,
-        0.9,
+        2380.39, 198.37, 0.16, 0.9
     )
 
     assert calculator([15000, 14000, 16000], 0.95871974, "Industrial") == (
-        33219.64,
-        2768.3,
-        0.15,
-        0.95,
+        33219.06, 2768.26, 0.15, 0.95
     )
 
     assert calculator([12000, 11000, 11400], 1.12307169, "Residencial") == (
-        40024.48,
-        3335.37,
-        0.22,
-        0.95,
+        40023.8, 3335.32, 0.22, 0.95
     )
 
     assert calculator([17500, 16000, 16400], 1.04820025, "Comercial") == (
-        46237.79,
-        3853.15,
-        0.18,
-        0.95,
+        46237.16, 3853.1, 0.18, 0.95
     )
 
     assert calculator([30000, 29000, 29500], 0.95871974, "Industrial") == (
-        63872.59,
-        5322.72,
-        0.18,
-        0.99,
+        63872.48, 5322.71, 0.18, 0.99
     )
 
     assert calculator([22000, 21000, 21400], 1.12307169, "Residencial") == (
-        74495.59,
-        6207.97,
-        0.25,
-        0.99,
+        74495.46, 6207.95, 0.25, 0.99
     )
 
     assert calculator([25500, 23000, 21400], 1.04820025, "Comercial") == (
-        66762.89,
-        5563.57,
-        0.22,
-        0.99,
+        66762.77, 5563.56, 0.22, 0.99
     )
 
     print("Everything passed")
+
